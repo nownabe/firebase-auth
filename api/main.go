3167ca1883afdacf51599927f32a181d8e65e029
nownabe <nownabe@gmail.com>
2018-08-14 00:26:18 +0900

    :tada: Authenticate with firebase

--- api/main.go
+++ api/main.go
@@ -1,11 +1,17 @@
 package main
 
 import (
+	"context"
+	"fmt"
 	"log"
 	"net/http"
+	"os"
+	"strings"
 
+	firebase "firebase.google.com/go"
 	"github.com/gorilla/handlers"
 	"github.com/gorilla/mux"
+	"google.golang.org/api/option"
 )
 
 func public(w http.ResponseWriter, r *http.Request) {
@@ -14,6 +20,35 @@ func public(w http.ResponseWriter, r *http.Request) {
 
 func private(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte("hello private"))
+}
+
+func authMiddleware(next http.HandlerFunc) http.HandlerFunc {
+	opt := option.WithCredentialsFile(os.Getenv("CREDENTIALS"))
+	app, err := firebase.NewApp(context.Background(), nil, opt)
+	if err != nil {
+		fmt.Printf("error: %v\n", err)
+		os.Exit(1)
+	}
+	auth, err := app.Auth(context.Background())
+	if err != nil {
+		fmt.Printf("error: %v\n", err)
+		os.Exit(1)
+	}
+
+	return func(w http.ResponseWriter, r *http.Request) {
+		authHeader := r.Header.Get("Authorization")
+		idToken := strings.Replace(authHeader, "Bearer ", "", 1)
+
+		token, err := auth.VerifyIDToken(context.Background(), idToken)
+		if err != nil {
+			fmt.Printf("error verifying ID token: %v\n", err)
+			w.WriteHeader(http.StatusUnauthorized)
+			w.Write([]byte("error verifying ID token"))
+			return
+		}
+		log.Printf("Verified ID token: %v\n", token)
+		next.ServeHTTP(w, r)
+	}
 }
 
 func main() {
@@ -25,7 +60,7 @@ func main() {
 
 	r := mux.NewRouter()
 	r.HandleFunc("/public", public)
-	r.HandleFunc("/private", private)
+	r.HandleFunc("/private", authMiddleware(private))
 
 	log.Fatal(http.ListenAndServe(":8000", corsHandler(r)))
 }
